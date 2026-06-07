f8ce3da4fbb96b2e65a5056e73d72412e64d1d2c
Koloa <koloa76777@gmail.com>
2026-06-08 00:00:20 +0800

    feat(Day7-Step1): SPI DMA non-blocking read AS5047P verified - Prescaler 16, Kick+TxRxCplt callback, NVIC TIM3=P0/SPI1=P2, spi_busy anti-reentry, smooth deg confirmed

--- Core/Inc/stm32f4xx_it.h
+++ Core/Inc/stm32f4xx_it.h
@@ -56,7 +56,10 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 void TIM3_IRQHandler(void);
+void SPI1_IRQHandler(void);
 void DMA2_Stream0_IRQHandler(void);
+void DMA2_Stream2_IRQHandler(void);
+void DMA2_Stream3_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
